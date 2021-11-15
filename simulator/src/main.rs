4e959e277022b1800e93357e15f7cdc6bbb9f2a2
Teodor <teo.virghi@yahoo.ro>
2021-11-15 21:55:07 +0200

    window init

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,3 +1,21 @@
+use nannou::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(init)
+        .update(update)
+        .simple_window(view)
+        .run();
+}
+
+struct Model {}
+
+fn init(_app: &App) -> Model {
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {
+}
+
+fn view(_app: &App, _model: &Model, frame: Frame){
+    frame.clear(WHITE);
 }
