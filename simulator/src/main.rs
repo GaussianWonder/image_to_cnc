0a82f1e379efdd717d38616799d3b42e61d69293
Teodor <teo.virghi@yahoo.ro>
2021-11-16 08:56:33 +0200

    colors helper, main template

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,21 +1,43 @@
+// #![allow(dead_code)]
+mod colors;
+pub use colors::*;
 use nannou::prelude::*;
 
 fn main() {
     nannou::app(init)
         .update(update)
-        .simple_window(view)
+        .event(event)
+        .view(view)
         .run();
 }
 
-struct Model {}
-
-fn init(_app: &App) -> Model {
-    Model {}
+struct Model {
+    window_id: window::Id,
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
+fn init(app: &App) -> Model {
+    let wnd = app.new_window().view(view).build().unwrap();
+    Model {
+        window_id: wnd,
+    }
 }
 
-fn view(_app: &App, _model: &Model, frame: Frame){
-    frame.clear(WHITE);
+/**
+ * Timed updates
+ */
+fn update(app: &App, model: &mut Model, update: Update) {
+}
+
+/**
+ * Window Events
+ * https://github.com/nannou-org/nannou/blob/master/examples/nannou_basics/all_functions.rs
+ */
+fn event(app: &App, model: &mut Model, event: Event) {
+}
+
+fn view(app: &App, model: &Model, frame: Frame){
+    frame.clear(DARK_GRAY);
+    let draw = app.draw();
+    draw.ellipse().color(STEELBLUE);
+    draw.to_frame(app, &frame).unwrap();
 }
