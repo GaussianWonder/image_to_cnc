471f54b318e971e7d549d82ede16b56c33090c0f
GaussianWonder <teo.virghi@yahoo.ro>
2021-12-27 21:48:55 +0200

    edge detection

---
+++ converter/README.md
@@ -0,0 +1,11 @@
+# converter
+
+## Run
+
+```bash
+cargo run --release --features display-window
+```
+
+```build
+cargo build --release --features display-window
+```
--- converter/src/main.rs
+++ converter/src/main.rs
@@ -1,3 +1,28 @@
+
+#[cfg(feature = "display-window")]
 fn main() {
-    println!("Hello, world!");
+    use imageproc::window::display_multiple_images;
+    use std::env;
+
+    let image_path = match env::args().nth(1) {
+        Some(path) => path,
+        None => {
+            println!("No image path provided. Using default image.");
+            "assets/image1.jpg".to_owned()
+        }
+    };
+
+    let image = image::open(&image_path)
+        .expect("No image found at provided path")
+        .grayscale()
+        .to_luma8();
+
+    let edges = imageproc::edges::canny(&image, 50.0f32, 60.0f32);
+
+    display_multiple_images("", &vec![&image, &edges], 500, 500);
+}
+
+#[cfg(not(feature = "display-window"))]
+fn main() {
+    panic!("Displaying images is only supported if the display-window feature is enabled.");
 }
