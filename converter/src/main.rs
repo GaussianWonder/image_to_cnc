2f2263a0cccc26a57d8d0726a8c9c999504f43b9
GaussianWonder <teo.virghi@yahoo.ro>
2021-12-29 09:36:56 +0200

    export image

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
         Some(path) => path,
         None => {
             println!("No image path provided. Using default image.");
-            "assets/image1.jpg".to_owned()
+            "assets/test.jpg".to_owned()
         }
     };
 
@@ -24,5 +24,32 @@ fn main() {
 
 #[cfg(not(feature = "display-window"))]
 fn main() {
-    panic!("Displaying images is only supported if the display-window feature is enabled.");
+    use std::env;
+
+    let image_path = match env::args().nth(1) {
+        Some(path) => path,
+        None => {
+            println!("No image path provided. Using default image.");
+            "assets/test.jpg".to_owned()
+        }
+    };
+
+    let image = image::open(&image_path)
+        .expect("No image found at provided path")
+        .grayscale()
+        .to_luma8();
+
+    let edges = imageproc::edges::canny(&image, 50.0f32, 60.0f32);
+
+    let result = image::save_buffer(
+        &image_path.replace(".jpg", "_edges.jpg"),
+        &edges,
+        edges.width() as u32,
+        edges.height() as u32,
+        image::ColorType::L8,
+    );
+
+    if result.is_err() {
+        println!("Error saving image: {:?}", result.err());
+    }
 }
